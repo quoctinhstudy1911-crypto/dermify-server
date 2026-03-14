e557e1a7cdf0414ee8256b4135ae97719e2fc83f
QT_BESAU <quoctinhstudy1911@gmail.com>
2026-03-14 10:53:28 +0700

    Fix demo users for api

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Dermify API Running...");
